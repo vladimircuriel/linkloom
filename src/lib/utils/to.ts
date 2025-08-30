3da34def337206624237c881e5192f78b852b417
NightmareVCO <vladimircuriel@outlook.com>
2025-08-29 21:34:02 -0400

    fix(utils): return original error in promise handler instead of wrapping it

--- src/lib/utils/to.ts
+++ src/lib/utils/to.ts
@@ -3,7 +3,7 @@ const to = async <T>(promise: Promise<T>): Promise<[T, null] | [null, Error]> =>
     const data = await promise
     return [data, null]
   } catch (error) {
-    return [null, new Error(`An error occurred: ${error}`)]
+    return [null, error as Error]
   }
 }
 
