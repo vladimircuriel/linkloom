cadfd575182817cbd8b4b53d1aacef9f8d04f5f5
NightmareVCO <vladimircuriel@outlook.com>
2025-08-29 21:31:59 -0400

    feat(auth): add error handling for undefined JWT secret in configuration

--- src/lib/constants/config.constants.ts
+++ src/lib/constants/config.constants.ts
@@ -1,6 +1,13 @@
 export const SALT_ROUNDS = process.env.NEXT_SALT_ROUNDS
 export const MONGO_DB_URI = process.env.NEXT_MONGO_DB_URI
 export const JWT_SECRET = process.env.NEXT_JWT_SECRET
+
+if (!JWT_SECRET) {
+  throw new Error(
+    'JWT secret is not defined. Please set NEXT_JWT_SECRET in your environment/config.',
+  )
+}
 export const ENCODED_JWT_SECRET = new TextEncoder().encode(JWT_SECRET)
+
 export const EXPIRATION_TIME = 60 * 60 * 24 // 1 day in seconds
 export const NODE_ENV = process.env.NODE_ENV
