5a052688e45346b0f114502a2611bdaff72d9f44
NightmareVCO <vladimircuriel@outlook.com>
2025-08-31 18:29:27 -0400

    feat: add loading component and enhance toast notifications for URL and user actions

--- src/lib/hooks/useUrlForm.tsx
+++ src/lib/hooks/useUrlForm.tsx
@@ -1,8 +1,48 @@
 import { createShortUrl } from '@lib/actions/url.action'
-import { useActionState } from 'react'
+import { useActionState, useEffect } from 'react'
+import toast from 'react-hot-toast'
 
 export default function useUrlForm() {
   const [state, formAction, isPending] = useActionState(createShortUrl, undefined)
+
+  useEffect(() => {
+    if (state?.success) {
+      toast('URL Created Successfully!', {
+        icon: '✅',
+        style: {
+          borderRadius: '10px',
+          background: '#060b14',
+          color: '#fff',
+        },
+      })
+    }
+  }, [state, state?.success])
+
+  useEffect(() => {
+    if (state?.error) {
+      toast.error(state.error, {
+        style: {
+          borderRadius: '10px',
+          background: '#060b14',
+          color: '#fff',
+        },
+      })
+    }
+  }, [state, state?.error])
+
+  useEffect(() => {
+    if (isPending) {
+      toast.loading('Creating short URL...', {
+        icon: '⏳',
+        duration: 1000,
+        style: {
+          borderRadius: '10px',
+          background: '#060b14',
+          color: '#fff',
+        },
+      })
+    }
+  }, [isPending])
 
   return { state, formAction, isPending }
 }
